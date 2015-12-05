abcd37f6c0a8c377c9244f290c1996bb172d67a7
rebeccaryang <rebeccayang123@gmail.com>
2015-12-04 20:46:17 -0800

    Completes basic requirements of watchout sprint

--- client/watchout.js
+++ client/watchout.js
@@ -6,6 +6,10 @@ var canvas = d3.select(".board")
   .style("width", 700)
   .style("background-color", "Pink");
 
+var collisions = 0;
+var highScore = 0;
+var currentScore = 0;
+
 // Setting up Enemy Locations
  var arr = [];
     for(var i = 0; i < 30; i++){
@@ -16,10 +20,28 @@ var canvas = d3.select(".board")
         move: function(){
           this.x = Math.random()*700;
           this.y = Math.random()*500;
+          currentScore+=5;
         }
       }
       arr.push(obj);
     }
+
+var updateCurrentScore = function(){
+  d3.select(".current")
+  .text("Current Score: " + currentScore);
+}
+
+var updateHighScore = function(){
+  if(currentScore > highScore){
+    highScore = currentScore;
+    d3.select(".highscore")
+    .text("High Score: "+ highScore)
+  }
+  collisions++;
+
+  d3.select(".collisions")
+  .text("Collisions: " + collisions);
+}
 
 // var playerArr = [];
 // playerArr[0] = {id: "player", x: 350, y: 250};
@@ -32,8 +54,6 @@ var player = canvas.append("circle")
     .attr("cy", 250)
     .attr("r",10)
 
-var mouseLoc;
-
 // var followMouse = function(){
 //   canvas.selectAll('circle.player')
 //   d3.behavior.drag()
@@ -45,13 +65,37 @@ var mouseLoc;
 //   .attr("cy", mouseLoc[1] || 250)
 // }
 
-var drag = d3.behavior.drag();
+var minX = 10;
+var maxX  = 690;
+var minY = 10;
+var maxY = 490;
+
+var drag = d3.behavior.drag()
+  .on("drag", function(d,i) {
+    if(d3.event.x < minX){
+      var x = minX;
+    } else if(d3.event.x > maxX){
+      var x = maxX;
+    } else {
+      var x = d3.event.x; 
+    }
+    if(d3.event.y < minY){
+      var y = minY;
+    } else if(d3.event.y > maxY){
+      var y = maxY;
+    } else{
+      var y = d3.event.y;
+    }
+    d3.select(this).attr('cx', x);
+    d3.select(this).attr('cy', y);
+  })
+
 canvas.select("circle.player").call(drag);
 
 //followMouse();
 
 // Setting up Enemies
-    canvas.selectAll("circle")
+    var enemy = canvas.selectAll("circle")
     .data(arr)
     .enter()
     .append("circle")
@@ -64,6 +108,24 @@ canvas.select("circle.player").call(drag);
       return d.y;
     })
     .attr("r", 10)
+    
+    console.log(enemy);
+
+    var detectCollision = function(){
+      enemy.each(function(){
+        var particularEnemy = d3.select(this);
+        playerX = canvas.select("circle.player").attr("cx")
+        enemyX = particularEnemy.attr("cx")
+        playerY = canvas.select("circle.player").attr("cy")
+        enemyY = particularEnemy.attr("cy")
+        if(Math.abs(playerX-enemyX) < 7 && Math.abs(playerY-enemyY) < 7){ 
+          updateHighScore();
+          currentScore = 0;
+          
+        }
+      })
+      updateCurrentScore();
+    }
 
     var enemies = function(){
       setInterval(function(){
@@ -81,6 +143,12 @@ canvas.select("circle.player").call(drag);
     }
 
 enemies();
+
+setInterval(detectCollision,50)
+
+// detectCollision();
+
+
 
     
   
